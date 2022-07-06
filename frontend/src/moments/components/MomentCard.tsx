5871c33a5f73a1a3fd31c426d2c9eb4cc2147c09
Ross Libby <rosslibby92@gmail.com>
2022-07-06 12:38:42 -0400

    set card focus when input selected

--- frontend/src/moments/components/MomentCard.tsx
+++ frontend/src/moments/components/MomentCard.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Moment } from "../data";
 import { LinkIcon } from '../../components/LinkIcon';
 import { TrashIcon } from '../../components/TrashIcon';
@@ -19,12 +19,33 @@ interface CardActionProps {
 
 const MomentCard = (props: MomentCardProps) => {
   // YOUR IMPLEMENTATION HERE
+  const cardRef = useRef(null)
+  const inputRef = useRef(null)
+  const [cardFocused, setCardFocused] = useState<boolean>(false)
+  const toggleCardFocus = (state?: boolean) => setCardFocused(state || !cardFocused)
   const handleRemoveCard = () => props.removeCard(props.moment.id)
 
+  useEffect(() => {
+    let currentCard = cardRef.current || null,
+      currentInput = inputRef.current || null
+
+    if (document.activeElement === currentInput) {
+      toggleCardFocus(true)
+    }
+
+    return () => {
+      currentCard = null
+      currentInput = null
+    }
+  }, [cardRef, inputRef, toggleCardFocus])
+
   return (
-    <div className="card" tabIndex={props.moment.id}>
+    <div className={`card ${cardFocused && 'card--focused'}`} tabIndex={props.moment.id} ref={cardRef} onBlur={() => toggleCardFocus(false)}>
       <span className={`card__type card__type--${props.moment.type}`}>{props.moment.type}</span>
       <div className="card__input"
+        onFocus={() => toggleCardFocus(true)}
+        ref={inputRef}
+        tabIndex={props.moment.id + 1}
         contentEditable={true}
         dangerouslySetInnerHTML={{__html: props.moment.notes}}
         onBlur={(e) => {
