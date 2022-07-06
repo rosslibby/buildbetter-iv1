f5ef27d46ac737dc4c08ab49d3fffb7d9f4c3036
Ross Libby <rosslibby92@gmail.com>
2022-07-06 11:21:10 -0400

    add type color variation

--- frontend/src/moments/components/MomentCard.tsx
+++ frontend/src/moments/components/MomentCard.tsx
@@ -11,7 +11,7 @@ const MomentCard = (props: MomentCardProps) => {
   // YOUR IMPLEMENTATION HERE
   return (
     <div className="card">
-      <span className="card__type">{props.moment.type}</span>
+      <span className={`card__type card__type--${props.moment.type}`}>{props.moment.type}</span>
       <input
         className="card__input"
         placeholder="Add note..."
